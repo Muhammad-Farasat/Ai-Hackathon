b91c61c8f491efb6b7dfe300632afc7dd77a4cd7
Muhammad-Farasat <farasatkhan687@gmail.com>
2025-07-13 15:06:27 +0500

    Chnages

--- Backend/db/connectToDb.js
+++ Backend/db/connectToDb.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectToDb = async() =>{
 
 mongoose.connect(process.env.MONGO_URL);
-
+// Chnages fr  git
 mongoose.connection.on('error', (err) => {
   console.log('MongoDB connection error:', err);
 });
